21469da82cc36c215c3183c65a5f0bb607ac1a4a
ivanChertan <thechertan@yandex.ru>
2025-04-29 22:07:23 +0300

    fix: обновить версию до 0.0.14 и изменить способ определения опциональных полей в ctr-config.model.ts

--- src/models/ctr-config.model.ts
+++ src/models/ctr-config.model.ts
@@ -5,8 +5,8 @@ export const CtrConfigSchema = z.object({
     secretWord1: z.string(),
     secretWord2: z.string(),
     shopId: z.number(),
-    payUrl: z.string().optional().default('https://pay.fk.money/'),
-    apiUrl: z.string().optional().default('https://api.fk.life/v1/'),
+    payUrl: z.optional(z.string().default('https://pay.fk.money/')),
+    apiUrl: z.optional(z.string().default('https://api.fk.life/v1/')),
     lang: z.enum(['ru', 'en']),
     currency: z.enum(['RUB', 'USD', 'EUR', 'UAH', 'KZT']),
 });
